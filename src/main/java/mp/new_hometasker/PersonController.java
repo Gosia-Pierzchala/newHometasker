0abbd4889d8340f3d5567d1a3b60383ae423592b
Gosia-Pierzchala <gk.kalinowska@gmail.com>
2018-09-30 17:35:04 +0200

    + personInfo

--- src/main/java/mp/new_hometasker/PersonController.java
+++ src/main/java/mp/new_hometasker/PersonController.java
@@ -1,11 +1,14 @@
 package mp.new_hometasker;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -55,4 +58,19 @@ public class PersonController {
         return "redirect:/users";
     }
 
+    @GetMapping("/person")
+    public String info(@RequestParam long id, Model model) {
+        Person person = homeTaskerRepository.findPersonById(id);
+        model.addAttribute("person", person);
+        List<Task> tasks = homeTaskerRepository.getTasks();
+        List<Task> assignedTasks = new ArrayList<>();
+        for (int j = 0; j < tasks.size(); j++) {
+            Task task = tasks.get(j);
+            if(person.equals(task.getPerson())){
+                assignedTasks.add(task);
+            }
+        }
+        model.addAttribute("tasks", assignedTasks);
+        return "personInfo";
+    }
 }
